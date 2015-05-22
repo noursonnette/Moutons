61a175c6b6c5df114d2baff7105fc13986770bb1
UGvDeathRain <jonathandu-68@hotmail.fr>
2015-05-22 14:59:52 +0200

    Create Plateau.java

---
+++ src/Plateau.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class Plateau {
+    protected int hauteurBas;
+    protected int hauteurHaut;
+    protected int hauteur;
+    protected int longueurGauche;
+    protected int longueurDroite;
+    protected int longueur;
+
+    protected ArrayList<PlaceCarte> tableau;
+
+    public Plateau() {
+        tableau = new ArrayList<PlaceCarte>();
+        hauteurBas = 5;
+        longueurDroite = 5;
+        hauteurHaut = 0 ;
+        longueurGauche = 0 ;
+        longueur = longueurGauche + longueurDroite ;
+        hauteur = hauteurBas + hauteurHaut ;
+
+        for (int i = 0; i < hauteur * longueur; i++) {
+            tableau.add(new PlaceCarte());
+        }
+    }
+
+    public void addCarte(int emplacementLongueur, int emplacementHauteur, Carte carte) throws mauvaiseLongueurException, mauvaiseHauteurException { //penser à la ligne 0 !
+        if (emplacementLongueur > longueur) {
+            throw new mauvaiseLongueurException("Longueur incorrecte");
+        }
+        if (emplacementHauteur > hauteur) {
+            throw new mauvaiseHauteurException("Hauteur incorrecte");
+        }
+        emplacementHauteur--;
+        tableau.get((emplacementHauteur * longueur + emplacementLongueur) - 1).setCarte(carte);
+    }
+
+    public Carte getCarte(int emplacementLongueur, int emplacementHauteur) {
+        return tableau.get(emplacementHauteur * hauteur + emplacementLongueur).getCarte();
+    }
+
+    public void augmenterHauteurBas() { // en bas
+        for (int i = 0; i < longueurDroite; i++) {
+            tableau.add(hauteurBas * longueur + i, new PlaceCarte());
+        }
+        hauteurBas++;
+        hauteur = hauteurBas + hauteurHaut ;
+    }
+
+    public void augmenterLongueurDroite() { // à droite
+        for (int i = hauteurBas * longueur; i > 0; i = i - longueur) {
+            tableau.add(i, new PlaceCarte());
+        }
+        longueurDroite++;
+        longueur = longueurGauche + longueurDroite ;
+    }
+
+    public void augmenterLongueurGauche() {
+        int compteur = 0;
+        for (int i = (hauteur * longueur) - longueur; i > 0; i = i - (longueur + compteur)) {
+            tableau.add(i, new PlaceCarte());
+            compteur = 1;
+        }
+        longueurGauche++;
+        longueur = longueurGauche + longueurDroite ;
+    }
+
+    public int getTaille() {
+        return tableau.size();
+    }
+
+    public void augmenterHauteurHaut() {
+        for (int i = longueur; i < 0; i--) {
+            tableau.add(0, new PlaceCarte()) ;
+        }
+        hauteurHaut++;
+        hauteur = hauteurBas + hauteurHaut ;
+    }
+}
