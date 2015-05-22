2b7f421f6da3f378370d2b75eb7e43f0d97d9082
EyesOfFallen <Choupi@MacBook-Air-de-Choupi.local>
2015-05-22 15:26:32 +0200

    Modification Pioche

--- src/Pioche.java
+++ src/Pioche.java
@@ -1,18 +1,22 @@
+import com.sun.xml.internal.bind.v2.TODO;
 import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Created by Charline & Kévin on 20/04/2015.
+ * Created by charline on 20/04/2015.
  */
 public class Pioche {
 
     ArrayList<Carte> listeCartes = new ArrayList<Carte>(77);
     private final int nbCartesTotal=77;
     private final int nbCartesInutiles=9;
+    private final int nbCartesPiochees=0;
 
     public Pioche(){
+
+        //TODO : Ajouter les chemins des cartes une fois scannés
 
         // Centre du village Position 0
         Carte c=new Carte();
@@ -98,12 +102,66 @@ public class Pioche {
         c.viderTabFace2();
 
 
+        // Chasseur rouge Position 13
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace2(0);
+        c.addElementFace2(0);
+        c.addElementFace2(0);
+        c.addElementFace2(0);
+        listeCartes.add(c);
+        c.viderTabFace1();
+        c.viderTabFace2();
+
+
+        // CHasseur bleu Position 14
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace2(1);
+        c.addElementFace2(1);
+        c.addElementFace2(1);
+        c.addElementFace2(1);
+        listeCartes.add(c);
+        c.viderTabFace1();
+        c.viderTabFace2();
+
+        // CHasseur jaune Position 15
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace2(2);
+        c.addElementFace2(2);
+        c.addElementFace2(2);
+        c.addElementFace2(2);
+        listeCartes.add(c);
+        c.viderTabFace1();
+        c.viderTabFace2();
+
+        // Chasseur noir Position 16
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace1(7);
+        c.addElementFace2(3);
+        c.addElementFace2(3);
+        c.addElementFace2(3);
+        c.addElementFace2(3);
+        listeCartes.add(c);
+        c.viderTabFace1();
+        c.viderTabFace2();
+
+
 
     }
 
     public Carte piocherCarte() {
         Random r = new Random();
-        int index=r.nextInt(nbCartesTotal-nbCartesInutiles)+nbCartesInutiles;
+        int index=r.nextInt(nbCartesTotal-nbCartesInutiles)+nbCartesInutiles-nbCartesPiochees;
         Carte c=this.listeCartes.get(index);
         this.listeCartes.remove(index);
         return c;
