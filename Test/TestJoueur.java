b12c40467dd131a539114ffb7fbc07a26304bdb9
noursonnette <charlinebersier@gmail.com>
2015-05-28 11:58:25 +0200

    Update TestJoueur.java

--- Test/TestJoueur.java
+++ Test/TestJoueur.java
@@ -2,7 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Created by Charline & Kévin on 20/04/2015.
+ * Created by charline on 20/04/2015.
  */
 public class TestJoueur {
 
@@ -11,27 +11,32 @@ public class TestJoueur {
         Joueur j = new Joueur("Dupont",0);
 
         Assert.assertEquals("Dupont",j.getNom());
+        Assert.assertNotEquals("Dup",j.getNom());
     }
 
     @Test
     public void testGetCouleur(){
         Joueur j = new Joueur("Dupont",0);
         Assert.assertEquals("rouge",j.getCouleur());
+        Assert.assertNotEquals("rou",j.getCouleur());
 
         Joueur j1 = new Joueur("Dupont",1);
         Assert.assertEquals("bleu",j1.getCouleur());
+        Assert.assertNotEquals("ble",j1.getCouleur());
 
         Joueur j2= new Joueur("Dupont",2);
         Assert.assertEquals("jaune",j2.getCouleur());
+        Assert.assertNotEquals("rou",j2.getCouleur());
 
         Joueur j3 = new Joueur("Dupont",3);
         Assert.assertEquals("noir",j3.getCouleur());
+        Assert.assertNotEquals("rou",j3.getCouleur());
     }
 
     @Test
     public void testPiocheUneCarte(){
         Joueur j1 = new Joueur("nom",4);
-        j1.piocheUneCarte();
+        j1.piocheCarte();
         int temp=1;
         Assert.assertEquals(j1.getPosCarte(),temp);
     }
@@ -46,7 +51,13 @@ public class TestJoueur {
 
     }
 
+    @Test
+    public void testAjouterUneCarte(){
 
+    }
 
+    @Test
+    public void testSupprimerUneCarte(){
 
+    }
 }
